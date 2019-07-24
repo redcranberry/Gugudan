23c0c994c76953bd2e0392242bb9c943a064da4d
Jungeun Kim <jungeun.kim@ericsson.com>
2019-07-24 13:15:58 +0900

    Gugudan while, for

---
+++ src/Gugudan.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Gugudan {
+
+    public static void main(String[] args) {
+        //while
+        //        System.out.println("What dan do you want?");
+        //        Scanner scanner = new Scanner(System.in);
+        //        int number = scanner.nextInt();
+        //        int j = 1;
+        //        int result;
+        //        while (j < 10) {
+        //            result = number * j;
+        //            System.out.println(result);
+        //            j = j + 1;
+        //        }
+        //for
+        System.out.println("What dan do you want?");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int result;
+        for (int j = 1; j < 10; j++) {
+            result = number * j;
+            System.out.println(result);
+            //add
+        }
+    }
+}
