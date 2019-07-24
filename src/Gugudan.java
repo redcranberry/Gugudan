b589363c3072bf77234ae16188023bce10beafbd
Jungeun Kim <jungeun.kim@ericsson.com>
2019-07-24 16:29:00 +0900

    Add if, else if, else

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -13,16 +13,27 @@ public class Gugudan {
         //            result = number * j;
         //            System.out.println(result);
         //            j = j + 1;
-        //        }
+        //                }
         //for
+        //        System.out.println("What dan do you want?");
+        //        Scanner scanner = new Scanner(System.in);
+        //        int number = scanner.nextInt();
+        //        int result;
+        //        for (int j = 1; j < 10; j++) {
+        //            result = number * j;
         System.out.println("What dan do you want?");
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
+        System.out.println("Input value : " + number);
         int result;
-        for (int j = 1; j < 10; j++) {
-            result = number * j;
-            System.out.println(result);
-            //add
+        if (number < 2) {
+            System.out.println("Please input bigger number than 2");
+        } else if (number > 9) {
+            System.out.println("Please input smaller number than 10");
+        } else {
+            for (int i = 1; i < 10; i++) {
+                System.out.println(number * i);
+            }
         }
     }
 }
